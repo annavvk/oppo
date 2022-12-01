64e469a1f6efea12a595077796ab05af26691475
annavvk <119591023+annavvk@users.noreply.github.com>
2022-12-01 19:36:44 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
-double my_pow(double num, unsigned int deg) {
+double my_pow(double num, int deg) {
     double result = 1;
-    for(long i = 0; i < deg; i++) {
-        result *= num;
+    if(deg < 0) 
+    {
+        deg = -deg;
+        for(long i = 0; i < deg; i++) 
+        {
+            result *= num;
+        }
+        return 1 / result;
     }
-    return result;
+    else {
+        for (long i = 0; i < deg; i++) {
+            result *= num;
+        }
+        return result;
+    }
 }
